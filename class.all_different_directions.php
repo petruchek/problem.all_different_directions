5ce2ed640df152fe90a770e9702b1266443ac69d
unknown <val.petruchek@gmail.com>
2019-02-21 16:46:04 +0100

    little bit of comments

--- class.all_different_directions.php
+++ class.all_different_directions.php
@@ -14,6 +14,12 @@ class All_Different_Directions
 	{
 	}
 
+	/**
+	 * Parse input string into array into special structure [x,y,[commands]]
+	 *
+	 * @param string $s one line containing both person's location [x,y] and that person's directions
+	 * @return array
+	 */
 	private function parse_direction($s)
 	{
 		//remove double spaces and tidy the string up
@@ -30,14 +36,28 @@ class All_Different_Directions
 		return $dir;
 	}
 
+	/**
+	 * Process direction from single person by parsing it and computing that person's direction.
+	 *
+	 * Saves destination coordinates [end_x, end_y] into that same array of that person.
+	 *
+	 * @param string $s one line containing both person's location [x,y] and that person's directions
+	 * @return void
+	 */
 	public function process_direction($s)
 	{
 		$dir = $this->parse_direction($s);
-	    $destination = $this->compute_direction($dir);
+		$destination = $this->compute_direction($dir);
 		list($dir['end_x'], $dir['end_y']) = $destination; 
 		$this->directions[] = $dir;
 	}
 
+	/**
+	 * Compute single person's destination from their position and set of commands
+	 *
+	 * @param array $dir [x,y,[commands]] special structure as returned by parse_direction() 
+	 * @return array [x,y] destination coordinates
+	 */
 	private function compute_direction($dir)
 	{
 		$x = $dir['x'];
@@ -66,11 +86,27 @@ class All_Different_Directions
 		return [$x, $y];
 	}
 
+	/**
+	 * The power of linear algebra - get distance between two points.
+	 *
+	 * @param float $x1
+	 * @param float $y1
+	 * @param float $x2
+	 * @param float $y2
+	 * @return float
+	 */
 	private function get_distance($x1,$y1,$x2,$y2)
 	{
 		return sqrt(($x1-$x2)**2+($y1-$y2)**2);
 	}
 
+	/**
+	 * 1. Calculate the average point of all advised destinations.
+	 *
+	 * 2. Calculate the worst offset among all advised destinations.
+	 *
+	 * @return array [x,y,worst]
+	 */
 	public function get_average_destination()
 	{
 		$n = count($this->directions);
